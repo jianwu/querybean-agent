d56d12d2907898651c27b9fef962f73a074b91be
Robin Bygrave <robin.bygrave@gmail.com>
2015-10-28 21:25:45 +1300

    #4 - Spring boot classLoader issue - java.lang.ClassFormatError: Illegal class name "Lorg/avaje/ebean/typequery/PLong;" in class file org/example/domain/query/QAddress

--- src/main/java/org/avaje/ebean/typequery/agent/FieldInfo.java
+++ src/main/java/org/avaje/ebean/typequery/agent/FieldInfo.java
@@ -4,6 +4,7 @@ import org.avaje.ebean.typequery.agent.asm.ClassVisitor;
 import org.avaje.ebean.typequery.agent.asm.Label;
 import org.avaje.ebean.typequery.agent.asm.MethodVisitor;
 import org.avaje.ebean.typequery.agent.asm.Opcodes;
+import org.avaje.ebean.typequery.agent.asm.Type;
 
 /**
  * Field information.
@@ -13,12 +14,14 @@ public class FieldInfo implements Opcodes, Constants {
   private final ClassInfo classInfo;
   private final String name;
   private final String desc;
+  private final String internalName;
   private final String signature;
 
   public FieldInfo(ClassInfo classInfo, String name, String desc, String signature) {
     this.classInfo = classInfo;
     this.name = name;
     this.desc = desc;
+    this.internalName = Type.getType(desc).getInternalName();
     this.signature = signature;
   }
 
@@ -51,7 +54,7 @@ public class FieldInfo implements Opcodes, Constants {
     mv.visitLabel(l2);
     mv.visitLineNumber(2, l2);
     mv.visitVarInsn(ALOAD, 0);
-    mv.visitTypeInsn(NEW, desc);
+    mv.visitTypeInsn(NEW, internalName);
     mv.visitInsn(DUP);
     mv.visitLdcInsn(name);
     mv.visitVarInsn(ALOAD, 0);
@@ -59,23 +62,23 @@ public class FieldInfo implements Opcodes, Constants {
     if (assocProperty) {
       if (typeQueryRootBean) {
         mv.visitInsn(ICONST_1);
-        mv.visitMethodInsn(INVOKESPECIAL, desc, "<init>", "(Ljava/lang/String;Ljava/lang/Object;I)V", false);
+        mv.visitMethodInsn(INVOKESPECIAL, internalName, "<init>", "(Ljava/lang/String;Ljava/lang/Object;I)V", false);
       } else {
         mv.visitFieldInsn(GETFIELD, classInfo.getClassName(), FIELD_ROOT, "Ljava/lang/Object;");
         mv.visitVarInsn(ALOAD, 0);
         mv.visitFieldInsn(GETFIELD, classInfo.getClassName(), FIELD_PATH, "Ljava/lang/String;");
         mv.visitInsn(ICONST_1);
-        mv.visitMethodInsn(INVOKESPECIAL, desc, "<init>", "(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/String;I)V", false);
+        mv.visitMethodInsn(INVOKESPECIAL, internalName, "<init>", "(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/String;I)V", false);
       }
 
     } else {
       if (typeQueryRootBean) {
-        mv.visitMethodInsn(INVOKESPECIAL, desc, "<init>", "(Ljava/lang/String;Ljava/lang/Object;)V", false);
+        mv.visitMethodInsn(INVOKESPECIAL, internalName, "<init>", "(Ljava/lang/String;Ljava/lang/Object;)V", false);
       } else {
         mv.visitFieldInsn(GETFIELD, classInfo.getClassName(), FIELD_ROOT, "Ljava/lang/Object;");
         mv.visitVarInsn(ALOAD, 0);
         mv.visitFieldInsn(GETFIELD, classInfo.getClassName(), FIELD_PATH, "Ljava/lang/String;");
-        mv.visitMethodInsn(INVOKESPECIAL, desc, "<init>", "(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/String;)V", false);
+        mv.visitMethodInsn(INVOKESPECIAL, internalName, "<init>", "(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/String;)V", false);
       }
     }
 
@@ -113,11 +116,11 @@ public class FieldInfo implements Opcodes, Constants {
     mv.visitLabel(l10);
     mv.visitLineNumber(3, l10);
     mv.visitVarInsn(ALOAD, 0);
-    mv.visitTypeInsn(NEW, desc);
+    mv.visitTypeInsn(NEW, internalName);
     mv.visitInsn(DUP);
     mv.visitLdcInsn(name);
     mv.visitVarInsn(ALOAD, 0);
-    mv.visitMethodInsn(INVOKESPECIAL, desc, "<init>", "(Ljava/lang/String;Ljava/lang/Object;)V", false);
+    mv.visitMethodInsn(INVOKESPECIAL, internalName, "<init>", "(Ljava/lang/String;Ljava/lang/Object;)V", false);
     mv.visitFieldInsn(PUTFIELD, classInfo.getClassName(), name, desc);
   }
 }
